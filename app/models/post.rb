8889f63ccc06e36852592e787c82523c11ef75bd
Victor Turitzin <turitzinv@gmail.com>
2022-07-09 20:02:39 -0400

    commit tree 52aadaa319cd4573aaf9d26e812e557abe41ff9a

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: ["Fiction", "Non-Fiction"] }
+  validate :title_clickbait
+
+  def title_clickbait
+    unless title.present? && title.include?("Won't Believe" || "Secret"|| "Top [number]"|| "Guess")
+      errors.add(:title, "Your title is not clickbait")
+    end
+  end
+
 end
